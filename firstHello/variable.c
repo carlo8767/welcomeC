d170651841a853667cb9869287690e7f7e41d7fb
carlo8767 <carlo8767@gmail.com>
2024-09-02 16:40:55 +0200

    variables_and_object

---
+++ firstHello/variable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// DECLARING A FUNCTION
+void sumUp(int a, int b){
+    int sum = a+b;
+    // SPÈCIFICY FORMAT DECIMAL
+    printf("%d",sum);
+}
+
+
+int main (void){
+
+    int a = 10;
+    int b = 11;
+    sumUp(a,b);
+    char h = 'hellochar';
+    int array[10], secondArray[10];
+ 
+}
