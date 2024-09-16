8cbb8004901b2c8fb1a3c5f34d6d0bd846e6b302
carlo8767 <carlo8767@gmail.com>
2024-09-16 11:59:07 +0200

    union_data_type

--- firstHello/customerStructure.c
+++ firstHello/customerStructure.c
@@ -1,14 +1,43 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
+
+struct customer *pointerCustomer;  // pointer to struct s
+
+// THE SIZE IS GREATER OF THE SIZE OF ITS MEMBER
 struct Customer {
     char name [1][15];
     int age;
-};
+} customer;
 
 
-int main (){
-    struct Customer firstCustomer = {"Carlo", 33};
-   // strcpy(firstCustomer.name , "Egzona");
-    printf("%s", firstCustomer.name[0]);
+// OCCUPY LESS MEMORY SIZE OF THE LARGEST MEMBER
+union SchoolStructur
+{
+    struct Student {
+        int id;
+        char name[1][15];
+    } student; // WITH THE TAG I ACCESS A THIS OBJCET
+} schoolStructure};
+
+
+
+
+int main (void){
+
+union SchoolStructur unionStudent;
+unionStudent.student.id = 33;
+schoolStructure.student.id = 29;
+union SchoolStructur secondUS;
+struct Student newStudent = {25, "John"};
+
+
+
+// ARRAY TYPE IS NOT ASSIGNABLE IN C
+strcpy(customer.name,"Pippo");
+printf("%s\n",customer.name[0]);
+// ISTANTIATE A STRCTURE
+struct Customer customerOne = {"Carlo",36};
+printf("%s\n",customerOne.name[0]);
+
 }
